7a9983b99e37b8e11430b111b6245f5f5c8c06d6
yasirpucit <malikyasir134@gmail.com>
2023-12-26 03:50:57 +0500

    added sample endpoint

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-import 'dotenv/config';
-import express from 'express';
+import "dotenv/config";
+import express from "express";
 
-import setupDatabase from './config/database';
-import applyMiddlewares from './middlewares';
-import router from './routes';
+import setupDatabase from "./config/database";
+import applyMiddlewares from "./middlewares";
+import router from "./routes";
 
 const app = express();
 
 setupDatabase();
 applyMiddlewares(app);
 
-
-app.use('/api', router);
+app.use("/api", router);
+app.use("/test-endpoint", (req, res) => {
+  return res.status(200).json({ message: "Hello From Node Server" });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`app is listening to port ${process.env.PORT}`);
