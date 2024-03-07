dc3810701e5f9ba05d3f6696f86bc8ed08d9bd69
yasirpucit <malikyasir134@gmail.com>
2024-03-08 04:06:15 +0500

    adding sample endpoint

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ setupDatabase();
 applyMiddlewares(app);
 
 app.use("/api", router);
+app.use("/test-endpoint", (req, res) => res.status(200).json({ message: "Server Is Listening" }));
 
 app.listen(process.env.PORT, () => {
   console.log(`app is listening to port ${process.env.PORT}`);
